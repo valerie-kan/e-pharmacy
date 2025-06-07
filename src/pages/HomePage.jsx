37092a8d0e5bdbb1c36d5b9c243a08d72d5d309c
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-06-07 16:50:27 +0300

    add offers section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero/Hero";
+import Offers from "../components/Offers/Offers";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <Offers />
     </div>
   );
 };
